380de52168d37e1fe4a062b640ad231a246590e7
Chenyang Yuan <chenyang.yuan@tri.global>
2025-09-23 16:50:19 -0400

    Added fid stats saving and loading

--- examples/cifar_unet.py
+++ examples/cifar_unet.py
@@ -7,6 +7,7 @@ import wandb
 from pathlib import Path
 
 from accelerate import Accelerator
+from pytorch_fid.fid_score import calculate_fid_given_paths, save_fid_stats
 from torch.utils.data import DataLoader
 from torchvision.datasets import CIFAR10
 from torchvision.utils import make_grid, save_image
@@ -21,7 +22,6 @@ from smalldiffusion import (
 )
 from smalldiffusion.data import JaxRandomSampler, img_test_transform
 
-
 def main(train_batch_size=256, epochs=1000, sample_batch_size=64, checkpoint=False):
     wandb.init()
     test_dataset = MappedDataset(
@@ -29,10 +29,8 @@ def main(train_batch_size=256, epochs=1000, sample_batch_size=64, checkpoint=Fal
                 transform=img_test_transform()),
         lambda x: x[0]
     )
-    TEST_DUMP = Path('datasets/cifar10-fid-dump')
-    dump_dataset(test_dataset, TEST_DUMP)
-
     a = Accelerator()
+    TEST_DUMP = dump_dataset(test_dataset, Path('datasets/cifar10-fid-test'), a)
 
     train_schedule = ScheduleSigmoid(N=1000)
     sample_schedule = ScheduleLogLinear(sigma_min=0.01, sigma_max=35, N=1000)
@@ -60,20 +58,23 @@ def main(train_batch_size=256, epochs=1000, sample_batch_size=64, checkpoint=Fal
             with ema.average_parameters():
                 torch.save(model.state_dict(), f'checkpoint-{i + 1:04}.pth')
                 fid = eval_fid(TEST_DUMP, a, model, sample_schedule,
-                        nfe=16, n=50_000,
+                        nfe=10, n=50_000,
                         batchsize=sample_batch_size)
                 print(f"FID at step {i}: {fid}")
                 wandb.log({'fid': fid}, step=i)
 
-    # Sample
-
-def dump_dataset(dataset, path):
+def dump_dataset(dataset, path, accelerator, batch_size=64):
     if not path.exists():
         print("Dumping test dataset...")
         path.mkdir(parents=True, exist_ok=True)
         counter = itertools.count()
         for i, img in zip(counter, dataset):
             save_image(img_normalize(img), path / f"{i:06}.png")
+
+    saved_path = path.with_name(f"{path.name}.npz")
+    if not saved_path.exists():
+        save_fid_stats([str(path), str(saved_path)], batch_size, accelerator.device, dims=2048)
+    return str(saved_path)
 
 def eval_fid(dataset_path, accelerator, model, eval_schedule, nfe, n, batchsize=64):
     with tempfile.TemporaryDirectory() as out_dir:
@@ -82,17 +83,12 @@ def eval_fid(dataset_path, accelerator, model, eval_schedule, nfe, n, batchsize=
         model.eval()
         counter = itertools.count()
         for _ in tqdm(range(n // batchsize), desc=f"Evaluating FID"):
-            *_, x0 = samples(model, eval_schedule.sample_sigmas(10), gam=2.1,
+            *_, x0 = samples(model, eval_schedule.sample_sigmas(nfe), gam=2.1,
                         batchsize=batchsize, accelerator=accelerator)
             for i, img in zip(counter, x0):
                 save_image(img_normalize(img), out_dir / f"{i:06}.png")
         model.train()
-        proc = subprocess.Popen(["pytorch-fid", str(out_dir), str(dataset_path)], stdout=subprocess.PIPE, shell=False)
-        for line in proc.stdout: # type: ignore
-            if b"FID:" in line:
-                return float(line.decode().split(":")[-1].strip())
-            print(line.decode(), end='')
-        raise RuntimeError("Could not parse FID output")
+        return calculate_fid_given_paths((str(out_dir), str(dataset_path)), batchsize, accelerator.device, dims=2048)
 
 if __name__=='__main__':
-    main()
+    main(checkpoint=True)
